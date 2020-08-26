52bf7a2bffc349b02b14dec4651fab943ab0369c
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-26 10:38:49 -0500

    Adding petitions to server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const router = express.Router()
 
 const app = express()
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(router)
 
 router.get('/message', function (req, res) {
+  console.log(req.headers)
+  res.header({
+    "custom-header": "Nuestro valor predeterminado"
+  })
   res.send('Lista de mensajes')
 })
 
 router.post('/message', function (req, res) {
-  res.send('Mensaje añadido')
+  console.log(req.query)
+  console.log(req.body)
+  res.status(201).send({
+    error: '',
+    body: 'Created successfully'
+  })
 })
 
 app.listen(3000);
