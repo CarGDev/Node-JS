e7792a05a3b6d9fd09e16eeab4731f94822e6d32
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-26 10:00:28 -0500

    Adding methods get and post

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ const app = express()
 
 app.use(router)
 
-router.get('/', function (req, res) {
-  res.send('hola')
+router.get('/message', function (req, res) {
+  res.send('Lista de mensajes')
 })
 
+router.post('/message', function (req, res) {
+  res.send('Mensaje añadido')
+})
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000')
