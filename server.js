2314bbe406961d2883826861b08db1288a787b50
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-26 11:09:50 -0500

    Adding an static HTML

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
 
+const response = require('./network/response')
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -12,17 +14,19 @@ router.get('/message', function (req, res) {
   res.header({
     "custom-header": "Nuestro valor predeterminado"
   })
-  res.send('Lista de mensajes')
+  response.success(req, res, 'Lista de mensajes')
 })
 
 router.post('/message', function (req, res) {
   console.log(req.query)
-  console.log(req.body)
-  res.status(201).send({
-    error: '',
-    body: 'Created successfully'
-  })
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'error simulado')
+  } else {
+    response.success(req, res, 'Creado correctamente', 201)
+  }
 })
+
+app.use('/app', express.static('public'))
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000')
