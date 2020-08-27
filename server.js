1c623a48c9efc5b78e9a9f1dac22505be6def4eb
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-26 21:58:06 -0500

    Adding webpack socket

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const route = require('./network/routers')
-
-const db = require('./db')
 const app = express()
+const server = require('http').Server(app)
+const route = require('./network/routers')
+const socket = require('./socket')
+const db = require('./db')
 const password = require('./components/message/password')
 const url = password.uri
 
 db.connect(url)
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+socket.connect(server)
 route(app)
 
 app.use('/app', express.static('public'))
 
-app.listen(3000);
+server.listen(3000, function () {
+  console.log('Server connected')
+});
 console.log('La aplicacion esta escuchando en http://localhost:3000')
